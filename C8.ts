1e97c890067f74eec401949533601f23caf6d903
trongnguyenhuynh55 <trongnguyenhuynh55@gmail.com>
2023-03-09 23:41:09 +0700

    C8

---
+++ C8.ts
@@ -0,0 +1,5 @@
+import { question } from "readline-sync";
+console.log ("Nhap a, b: ");
+let x: number = Number(question("Nhap a: "));
+let y: number = Number(question("Nhap b: "));
+console.log ("a = " + y +" , b = " + x);
